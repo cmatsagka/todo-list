161a8797dee1f7ff44cc1f89ebe87f2799785561
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-02-04 20:07:05 +0200

    Create factory function for todo objects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './styles.css';
+import { createTodo } from './todo.js';
 
-console.log('DOM Loaded');
+const todo1 = createTodo(
+	'say hi',
+	'say hi after making todo',
+	'6/2/2025',
+	'medium'
+);
+const todo2 = createTodo(
+	'say thanks',
+	'say thanks after hearing something nice',
+	'8/2/2025',
+	'high'
+);
+
+console.log(todo1);
+console.log(todo2);
