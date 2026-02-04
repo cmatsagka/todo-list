63c84dbd596909f36fc9e56e854d5549761880f5
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-02-04 21:29:07 +0200

    Add button and text field in ui

--- src/domDisplay.js
+++ src/domDisplay.js
@@ -1,18 +1,27 @@
 import { addProject, getAllProjects } from './todoManager.js';
 
-const content = document.querySelector('#content');
-
 export function setInitialUI(name) {
+	const content = document.querySelector('#content');
+
+	const addContainer = document.createElement('div');
+	addContainer.classList.add('add-container');
+
+	const input = document.createElement('input');
+	input.placeholder = 'New Project Name';
+	input.id = 'project-input';
+
 	const addButton = document.createElement('button');
 	addButton.classList.add('add-btn');
 	addButton.textContent = 'Add Project';
 
 	addButton.addEventListener('click', () => {
 		addProject(name);
+		renderProjects();
 	});
 
-	renderProjects();
-	content.appendChild(addButton);
+	addContainer.appendChild(input);
+	addContainer.appendChild(addButton);
+	content.appendChild(addContainer);
 }
 
 export function renderProjects() {
