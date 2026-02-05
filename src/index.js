7b3c34a9f53e1758d96232133c4c8f01fe908cda
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-02-05 20:47:27 +0200

    Remove code

--- src/index.js
+++ src/index.js
@@ -4,23 +4,6 @@ import { createProject } from './project.js';
 import { renderProjects, renderTodos, setUI } from './domDisplay.js';
 import { addProject, deleteProject, getAllProjects } from './todoManager.js';
 
-const todo1 = createTodo(
-	'say hi',
-	'say hi after making todo',
-	'6/2/2025',
-	'medium'
-);
-const todo2 = createTodo(
-	'say thanks',
-	'say thanks after hearing something nice',
-	'8/2/2025',
-	'high'
-);
-
-const general = getAllProjects()[0];
-general.addTodo(todo1);
-general.addTodo(todo2);
-
 setUI();
 renderProjects();
 renderTodos();
