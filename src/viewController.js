3b88e83940bc0147c727f48ec314aa54233ebe37
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-02-15 15:15:23 +0200

    Create new file that works as a controller for changing views

---
+++ src/viewController.js
@@ -0,0 +1,19 @@
+let viewDashBoard = 'DASHBOARD';
+let viewSingleProject = 'SINGLE';
+
+let currentView = viewDashBoard;
+
+export function switchView(viewType, contextData) {
+	const mainArea = document.querySelector('#todo-items');
+	mainArea.textContent = '';
+
+	switch (viewType) {
+		case viewDashBoard:
+			createBoard(contextData);
+			break;
+		case viewSingleProject:
+			createList(contextData);
+	}
+
+	currentView = viewType;
+}
