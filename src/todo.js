37c8f40a4da7722b9999df2610b48aeb9466b3b0
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-02-16 01:14:38 +0200

    Add a complete feature in todo

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,18 @@
-export function createTodo(title, description, dueDate, priority) {
-	return { title, description, dueDate, priority };
+export function createTodo(
+	title,
+	description,
+	dueDate,
+	priority,
+	completed = false
+) {
+	return {
+		title,
+		description,
+		dueDate,
+		priority,
+		completed,
+		toggleComplete() {
+			this.completed = !this.completed;
+		},
+	};
 }
